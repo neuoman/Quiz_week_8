42dab02102fca81b683da08b8e2adddb4310b45b
neuoman <neuoman3188@gmail.com>
2023-09-16 22:16:10 +1000

    Q2 file updated

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+# Load data from the CSV file using pandas, specifying the delimiter as a comma
+data = pd.read_csv('your_csv_file.csv', delimiter=',')
 
+# Extract data into separate lists
+years = data['Year']
+co2 = data['CO2']
+temp = data['Temperature']
+
+# Create subplots
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+# Show the figure
+plt.tight_layout()
+plt.show()
+
+# Save the figure as an image
+plt.savefig("co2_temp_2.png")
+
 
